9a4867da7faba36e3d1f49d9bfb3b10fefd458cb
Pierre Chifflier <chifflier@wzdftpd.net>
2018-05-15 10:00:02 +0200

    Rewrite and simplify extensions and MAC parsing

--- src/ntp.rs
+++ src/ntp.rs
@@ -1,4 +1,4 @@
-use nom::{be_i8,be_u8,be_u16,be_u32,be_u64};
+use nom::{be_i8,be_u8,be_u16,be_u32,be_u64,ErrorKind,IResult};
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct NtpMode(pub u8);
@@ -31,8 +31,8 @@ pub struct NtpPacket<'a> {
     pub ts_recv:u64,
     pub ts_xmit:u64,
 
-    pub ext_and_auth:Option<(Vec<NtpExtension<'a>>, (u32, &'a[u8]))>,
-    pub auth: Option<(u32,&'a[u8])>,
+    pub extensions:Vec<NtpExtension<'a>>,
+    pub auth: Option<NtpMac<'a>>
 }
 
 impl<'a> NtpPacket<'a> {
@@ -49,8 +49,15 @@ pub struct NtpExtension<'a> {
     /*padding*/
 }
 
-named!(pub parse_ntp_extension<NtpExtension>,
+#[derive(Debug,PartialEq)]
+pub struct NtpMac<'a> {
+    pub key_id: u32,
+    pub mac:    &'a[u8],
+}
+
+pub fn parse_ntp_extension(i: &[u8]) -> IResult<&[u8],NtpExtension> {
     do_parse!(
+        i,
         field_type: be_u16 >>
         length:     be_u16 >> // len includes the padding
         value:      take!(length) >>
@@ -60,12 +67,43 @@ named!(pub parse_ntp_extension<NtpExtension>,
                 length,
                 value,
             }
-        ))
-);
+        )
+    )
+}
 
-named!(pub parse_ntp_key_mac<(u32,&[u8])>,
-   complete!(pair!(be_u32,take!(16)))
-);
+fn parse_ntp_mac(i: &[u8]) -> IResult<&[u8],NtpMac> {
+   do_parse!(
+       i,
+       key_id: be_u32 >>
+       mac:    take!(16) >>
+       ( NtpMac{ key_id, mac} )
+   )
+}
+
+// optional fields, See section 7.5 of [RFC5905] and [RFC7822]
+// extensions, key ID and MAC
+//
+// check length: if == 20, only MAC
+//               if >  20, ext + MAC
+//               if ==  0, nothing
+//               else      error
+fn parse_extensions_and_auth(i:&[u8]) -> IResult<&[u8],(Vec<NtpExtension>,Option<NtpMac>)> {
+    if i.is_empty() { IResult::Done(i,(Vec::new(),None)) }
+    else if i.len() == 20 {
+        parse_ntp_mac(i).map(|m| (Vec::new(),Some(m)))
+    }
+    else if i.len() > 20 {
+        do_parse!(
+            i,
+            v: many1!(complete!(parse_ntp_extension)) >>
+            m: parse_ntp_mac >>
+               eof!() >>
+            ( (v,Some(m)) )
+        )
+    } else {
+        IResult::Error(error_code!(ErrorKind::Eof))
+    }
+}
 
 named!(pub parse_ntp<NtpPacket>,
    do_parse!(
@@ -82,10 +120,7 @@ named!(pub parse_ntp<NtpPacket>,
        ts_orig:         be_u64 >>
        ts_recv:         be_u64 >>
        ts_xmit:         be_u64 >>
-       // optional fields, See section 7.5 of [RFC5905] and [RFC7822]
-       // extensions, key ID and MAC
-       ext_and_auth:    opt!(complete!(pair!(many0!(complete!(parse_ntp_extension)),parse_ntp_key_mac))) >>
-       auth: opt!(parse_ntp_key_mac) >>
+       ext_and_auth:    parse_extensions_and_auth >>
        (
            NtpPacket {
                li:b0.0,
@@ -101,8 +136,8 @@ named!(pub parse_ntp<NtpPacket>,
                ts_orig,
                ts_recv,
                ts_xmit,
-               ext_and_auth,
-               auth,
+               extensions: ext_and_auth.0,
+               auth: ext_and_auth.1
            }
    ))
 );
@@ -137,7 +172,7 @@ fn test_ntp_packet1() {
         ts_orig:0,
         ts_recv:0,
         ts_xmit:14195914391047827090u64,
-        ext_and_auth:None,
+        extensions:Vec::new(),
         auth:None,
     });
     let res = parse_ntp(&bytes);
@@ -171,8 +206,8 @@ fn test_ntp_packet2() {
         ts_orig:0,
         ts_recv:0,
         ts_xmit:14710388140573593600,
-        ext_and_auth:None,
-        auth:Some((1,&bytes[52..])),
+        extensions:Vec::new(),
+        auth:Some(NtpMac{key_id:1,mac:&bytes[52..]}),
     });
     let res = parse_ntp(&bytes);
     assert_eq!(res, expected);
